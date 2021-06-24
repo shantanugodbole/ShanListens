46639620bd6621f1dce224c6eb78a8d867af8cf7
Shantanu Godbole <46984743+shantanugodbole@users.noreply.github.com>
2021-06-24 11:58:29 +0530

    added spotify script

---
+++ main.py
@@ -0,0 +1,58 @@
+import requests
+import time
+from flask import Flask, request
+from pprint import pprint
+
+app = Flask(__name__)
+
+SPOTIFY_GET_CURRENT_TRACK_URL = 'https://api.spotify.com/v1/me/player/currently-playing'
+ACCESS_TOKEN = 'BQBmbDSI0qoZPlKNv6JN_A_BKl-zWlh92kRBmBUFVYg0e4D3SQ3R1hfhRvBQNYxH53oPCXT_MJ7AOXCWvHDEOmfPOs5BDnUh1sHwZKDka7_1MWNcwAFtjCpt0pEgOKHUb8uiidFumVqESwZo6CO6OSZ9QKgnbDSXhUmyHl2kZw'
+
+
+@app.route('/test/<test>')
+def default(test):
+    return "Testing Flask App" + test
+
+
+@app.route('/get_song')
+def getSongInfo():
+    current_track_id = None
+    while True:
+        current_track_info = get_current_track(ACCESS_TOKEN)
+        if current_track_info['id'] != current_track_id:
+            pprint(current_track_info,indent=4)
+            current_track_id = current_track_info['id']
+            return current_track_info
+    time.sleep(200)
+    
+def get_current_track(access_token):
+    response = requests.get(
+        SPOTIFY_GET_CURRENT_TRACK_URL,
+        headers={
+            "Authorization": f"Bearer {access_token}"
+        }
+    )
+    json_resp = response.json()
+
+    track_id = json_resp['item']['id']
+    track_name = json_resp['item']['name']
+    artists = [artist for artist in json_resp['item']['artists']]
+
+    link = json_resp['item']['external_urls']['spotify']
+
+    artist_names = ', '.join([artist['name'] for artist in artists])
+
+    current_track_info = {
+    	"id": track_id,
+    	"track_name": track_name,
+    	"artists": artist_names,
+    	"link": link
+    }
+
+    return current_track_info
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
